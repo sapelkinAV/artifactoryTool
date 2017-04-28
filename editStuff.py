18dd227bdb3a88a154689664636f806fab3acf11
Alexander Sapelkin <al.vi.sapelkin@gmail.com>
2017-04-28 04:40:52 +0300

    fix problems

--- editStuff.py
+++ editStuff.py
@@ -1,106 +1,49 @@
-import os, zipfile, shutil, glob
-from pathlib import Path
-
-import sys
+import os, zipfile, shutil, glob, shutil
 
 
-def unzipFolders(path):
-    extension = ".zip"
-    for item in os.listdir(path):  # loop through items in dir
-        if item.endswith(extension):  # check for ".zip" extension
-            file_name = os.path.abspath(item)  # get full path of files
-            zip_ref = zipfile.ZipFile(file_name)  # create zipfile object
-            zip_ref.extractall(path)  # extract file to dir
-            zip_ref.close()  # close file
-            os.remove(file_name)  # delete zipped file
+dir_name = os.path.realpath("./testArchieves")
 
 
-def zipdir(dirPath=None, zipFilePath=None, includeDirInZip=True):
-    if not zipFilePath:
-        zipFilePath = dirPath + ".zip"
-    if not os.path.isdir(dirPath):
-        raise OSError("dirPath argument must point to a directory. "
-                      "'%s' does not." % dirPath)
-    parentDir, dirToZip = os.path.split(dirPath)
+path_to_archieve = []
 
-    # Little nested function to prepare the proper archive path
-    def trimPath(path):
-        archivePath = path.replace(parentDir, "", 1)
-        if parentDir:
-            archivePath = archivePath.replace(os.path.sep, "", 1)
-        if not includeDirInZip:
-            archivePath = archivePath.replace(dirToZip + os.path.sep, "", 1)
-        return os.path.normcase(archivePath)
+def unzipStuff(workPath):
+    for path, subdirs, files in os.walk(dir_name):
+        for name in files:
+            if name.endswith(".zip"):
 
-    outFile = zipfile.ZipFile(zipFilePath, "w",
-                              compression=zipfile.ZIP_DEFLATED)
-    for (archiveDirPath, dirNames, fileNames) in os.walk(dirPath):
-        for fileName in fileNames:
-            filePath = os.path.join(archiveDirPath, fileName)
-            outFile.write(filePath, trimPath(filePath))
-        # Make sure we get empty directories as well
-        if not fileNames and not dirNames:
-            zipInfo = zipfile.ZipInfo(trimPath(archiveDirPath) + "/")
-            # some web sites suggest doing
-            # zipInfo.external_attr = 16
-            # or
-            # zipInfo.external_attr = 48
-            # Here to allow for inserting an empty directory.  Still TBD/TODO.
-            outFile.writestr(zipInfo, "")
-    outFile.close()
+                path_to_archieve.append((os.path.join(path, name).replace('.zip', '')))
+
+                unzippingFile = zipfile.ZipFile(os.path.join(path, name))
+                unzippingFile.extractall(os.path.join(path, name).replace('.zip', ''))
+                os.remove(os.path.join(path, name))
 
 
-def zipFolders(path):
-    for item in os.listdir(path):
-        if os.path.isdir(item):
-            file_name = os.path.abspath(item)
-            zipdir(file_name)
-
-    # delete folders
-    for item in os.listdir(path):
-        if os.path.isdir(item):
-            shutil.rmtree(item)
-
+def zipFiles():
+    for element in path_to_archieve:
+        print(element)
+        shutil.make_archive(element,'zip',element)
+        shutil.rmtree(element)
 
 def modifyBuildGradle(pathToBuildGradle):
     stringsToDelete = ['classpath \"jt400:jt400:1.0.0\"', 'classpath \"ccas400:ccas400:1.0.0\"']
-
+    lines = []
     with open(pathToBuildGradle, "r") as file:
         lines = file.readlines()
 
     with open(pathToBuildGradle, "w") as file:
         for line in lines:
             if not any(filter_word in line for filter_word in stringsToDelete):
-                file.write(
-                    line.replace("url \"http://mvn/artifactory/releases\"", "url \"http://mvn/artifactory/public\""))
+                line = line.replace("url \"http://mvn/artifactory/releases\"", "url \"http://mvn/artifactory/public\"")
+                file.write(line)
 
-
-def findFileByName(name, path):
-    for root, dirs, files in os.walk(path):
-        if name in files:
-            return os.path.join(root, name)
-
-def editFolders(path):
-    listdirs = [x[0] for x in os.walk(path)]
-    listdirs.pop(0)
-    for folder in listdirs:
-        if os.path.isdir(folder):
-            build_gradle = findFileByName('build.gradle',folder)
-            if build_gradle is not None and os.path.isfile(build_gradle):
-                modifyBuildGradle(build_gradle)
+def editFiles():
+    for path, subdirs, files in os.walk(dir_name):
+        for name in files:
+            if name == 'build.gradle':
+                modifyBuildGradle(os.path.join(path,name))
 
 
 
-
-
-
-
-
-dir_name = os.path.realpath('./testArchieves/')
-
-if __name__ == '__main__':
-    dir_name = os.path.realpath(sys.argv[1])
-    os.chdir(dir_name)
-    unzipFolders(dir_name)
-    editFolders(dir_name)
-    zipFolders(dir_name)
+unzipStuff(dir_name)
+editFiles()
+zipFiles()
